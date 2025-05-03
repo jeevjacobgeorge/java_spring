b8a2f8171c7e8a05ceace12f5e83346030992c9c
jeevjacobgeorge <jeevjacobgeoge@gmail.com>
2025-05-03 21:08:38 +0530

    h2 db connected

---
+++ demo/src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    public DataLoader(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        userRepository.save(new User(null, "Alice"));
+        userRepository.save(new User(null, "Bob"));
+    }
+}
--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -1,13 +1,21 @@
 package com.example.demo;   
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
-
+import java.util.List;
+import org.springframework.ui.Model;
 @Controller
 public class DemoController {
     
-      @GetMapping("/")
-      public String home(){
+  @Autowired
+  private UserRepository userRepository;
+  
+  @GetMapping("/")
+  public String home(Model model){
+        List<User> users = userRepository.findAll();
+        
+        model.addAttribute("users",users);
         return "index";
-      }
+  }
 }
